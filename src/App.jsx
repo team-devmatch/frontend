a1cdb57757b555cf81b5cf8490a68c9ad4fa5ecf
kimjihwan <p1nkhwan1002@gmail.com>
2026-04-22 17:15:06 +0900

    헤더 컴포넌트 완성

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/reset.css";
 import "./styles/global.css";
 import IndexPage from "./pages/IndexPage";
 import Header from "./components/common/Header";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const menuList = [
+    { name: "축제행사", path: "/festival" },
+    { name: "캘린더", path: "/calendar" },
+    { name: "AI추천", path: "/ai" },
+  ];
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>
+          <img src={logo} alt="로고" />
+        </div>
+
+        <nav className={styles.navCenter}>
+          <ul>
+            {menuList.map((menu) => (
+              <li key={menu.path}>
+                <Link to={menu.path}>{menu.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className={styles.navRight}>
+          <ul>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="register">회원가입</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

f371481ee9134a86400f51dc8ec271ea0a65f758
kimjihwan <p1nkhwan1002@gmail.com>
2026-04-23 18:55:58 +0900

    헤더css수정

--- src/api/festivalApi.js
+++ src/api/festivalApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getRecentFestivals = async () => {
   try {
-    const res = await axios.get("http://localhost:8080/festivals/recent");
+    const res = await axios.get("http://192.168.0.179:8080/festivals/recent");
     return res.data;
   } catch (error) {
     console.error("API 에러:", error);

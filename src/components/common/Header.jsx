36dd6f932e7688feba5818ca0b942383d25427b6
ohchunghwan <ohch9712@gmail.com>
2026-04-23 10:09:40 +0900

    jihwanhwan님 헤더 가져오기

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const menuList = [
+    { name: "축제행사", path: "/festival" },
+    { name: "캘린더", path: "/calendar" },
+    { name: "게시판", path: "/board" },
+  ];
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY < 100) {
+        setIsVisible(true);
+      } else if (currentScrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
+  return (
+    <header
+      className={`${styles.header} ${isVisible ? styles.show : styles.hide}`}
+    >
+      <div className={styles.inner}>
+        <div className={styles.logo}>
+          <Link to="/">
+            <img src={logo} alt="로고" />
+          </Link>
+        </div>
+
+        <nav className={styles.navCenter}>
+          <ul>
+            {menuList.map((menu) => (
+              <li key={menu.path}>
+                <Link to={menu.path}>{menu.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className={styles.navRight}>
+          <ul>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/signup">회원가입</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

50becc3b4722b41bec9a4deebd7c6ef5f04acb49
kimjihwan <p1nkhwan1002@gmail.com>
2026-04-24 11:27:17 +0900

    유네스코 캐러셀 구현

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import CourseCarousel from "../components/courseCarousel/courseCarousel";
+import UnescoCarousel from "../components/unescoCarousel/unescoCarousel";
 
 const IndexPage = () => {
   return (
     <>
       <Banner />
       <CourseCarousel />
+      <UnescoCarousel />
     </>
   );
 };

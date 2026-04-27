a27b043575671e9f44ed3dd87d3f86b3d7837bab
kyungjunoh <orudwns11@naver.com>
2026-04-27 12:35:51 +0900

    AI 챗봇 UI 구현(아이콘,트리거,팝업채팅창)

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./styles/global.css";
 import IndexPage from "./pages/IndexPage";
 import Header from "./components/common/Header";
 import { Routes } from "react-router-dom";
+import ChatBot from './components/chatbot/ChatBot';
 
 function App() {
   return (
     <>
       <Header />
       <IndexPage />
+      <ChatBot />
     </>
   );
 }
